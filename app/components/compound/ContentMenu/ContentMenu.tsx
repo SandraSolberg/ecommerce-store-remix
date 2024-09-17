b22d0e13700e56953ca7e74211ed98df378b5571
Sandra Solberg <sandsol97@gmail.com>
2024-09-17 13:28:33 +0200

    Fixed styling on categories

--- app/components/compound/ContentMenu/ContentMenu.tsx
+++ app/components/compound/ContentMenu/ContentMenu.tsx
@@ -10,19 +10,20 @@ const ContentMenu = () => {
     : null;
 
   return (
-    <div className='bg-white absolute w-full border border-y-gray-400 flex flex-col p-6'>
-      <ul>
-        <li className='px-2 pb-2 font-semibold'>
-          <button className='hover:underline'>Show all products</button>
-        </li>
-      </ul>
-      <ul className='list'>
-        {parentCategories?.map((category) => (
-          <li key={category.foodGroupId}>
-            <button>{category.name}</button>
-          </li>
-        ))}
-      </ul>
+    <div className='bg-white absolute w-full border border-y-gray-400 flex flex-col py-6 '>
+      <div className='max-w-[1400px] m-auto'>
+        <button className='hover:underline px-2 pb-2 font-semibold'>
+          Show all products
+        </button>
+
+        <ul className='list'>
+          {parentCategories?.map((category) => (
+            <li key={category.foodGroupId}>
+              <button>{category.name}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
